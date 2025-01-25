d41386151266599da2c86cb18915334853226354
TeslaCuil <pearcem.dx@gmail.com>
2025-01-24 21:24:28 -0500

    more bubbles

--- bubble.gd
+++ bubble.gd
@@ -1,8 +1,15 @@
 class_name Bubble
 extends RigidBody2D
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+@onready var panel: Panel = $Panel
+
 var random_force : Vector2
 
 func _ready() -> void:
 
 	apply_central_impulse(random_force)
+
+func set_bubble_size(factor : float):
+	collision_shape_2d.scale *= factor
+	panel.scale *= factor
--- bubble_spawner.gd
+++ bubble_spawner.gd
@@ -1,3 +1,20 @@
+class_name BubbleSpawner
 extends Node2D
 
-@export bubble_object
+@onready var timer: Timer = $"../Timer"
+
+@export var bubble_object : PackedScene
+@export var bubble_cap : int = 50
+
+var rng = RandomNumberGenerator.new()
+var bubble_count : int = 0
+
+func _on_timer_timeout() -> void:
+	if bubble_count < bubble_cap:
+		var bubble = bubble_object.instantiate() as Bubble
+		bubble.random_force = Vector2(randi_range(-20,20),randi_range(-20,20))
+		
+		add_child(bubble)
+		bubble.set_bubble_size(randf_range(0.5,2))
+		bubble_count += 1
+		timer.start()
