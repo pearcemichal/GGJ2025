679ce612da7796784506b23010f60c15a148dc30
TeslaCuil <pearcem.dx@gmail.com>
2025-01-25 18:58:57 -0500

    added end game screen

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -1,21 +1,26 @@
 extends Node2D
 
+@onready var victory_screen: Panel = %VictoryScreen
+@onready var end_message: Label = %EndMessage
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	victory_screen.visible = false
+	SignalBus.PlayerKrill.connect(_on_player_krill)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_BACKSPACE):
-		get_tree().reload_current_scene();
-	if Input.is_key_pressed(KEY_ESCAPE):
-		get_tree().quit()
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"): get_tree().reload_current_scene();
+	if event.is_action_pressed("quit"): get_tree().quit()
+		
 
 func _on_killzone_body_entered(body: Node2D) -> void:
 	if body.is_in_group("players"):
 		body.position = Vector2.ZERO
-		body.reset_state();
 	if body.is_in_group("krillable"):
 		body.queue_free()
+		
+func _on_player_krill(player_id : int):
+	victory_screen.visible = true
+	end_message.text = "Player %s has been Krilled" % player_id 
+	
--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -1,18 +1,43 @@
 extends Control
 
-@onready var label: Label = $Label
+@onready var timer: Timer = $Timer
+@onready var health_bar: HBoxContainer = $HealthBar
 
-@export var health : int = 10
+@export var health : int = 0
 @export var player_id : int
+
+var button_array : Array[TextureButton]
+
+var invulnerable : bool = false
+var dead : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	label.text = str(health)
+	for child in health_bar.get_children():
+		health += 1
+		button_array.append(child.get_child(0))
+	
 	SignalBus.PlayerTakeDamage.connect(_on_player_damaged)
+	
+	health = 2
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _on_player_damaged(id: int) -> void:
-	if id == player_id:
-		health -= 1
-		print("P%s health is now %s" % [player_id, health])
-		label.text = "P%s|%s" % [player_id, health]
+	
+	if !invulnerable && !dead:
+		if id == player_id:
+			health -= 1
+			button_array[health].disabled = true
+
+		else:
+			invulnerable = true
+			timer.start()
+			
+	if (health <= 0):
+		dead = true
+		SignalBus.PlayerKrill.emit(player_id)
+
+
+
+func _on_timer_timeout() -> void:
+	invulnerable = false
