c3d124f0f8888c359542dbb4c333e27b7d090cc6
TeslaCuil <pearcem.dx@gmail.com>
2025-01-25 14:16:18 -0500

    new logic for hitting your own bubbles

--- scripts/bubble_life.gd
+++ scripts/bubble_life.gd
@@ -10,6 +10,7 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.is_in_group("popper"):
 		if body.is_in_group("players") && body.player_id != player_id:
 			SignalBus.PlayerTakeDamage.emit(player_id)
+			pop_da_bubble()
 		else:
-			body.velocity = -body.velocity / 3
-		pop_da_bubble()
+			apply_central_force(body.velocity * 2)
+		
