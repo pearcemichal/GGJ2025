f866a763d5bc83789f35c3865305c850d0cd4045
TeslaCuil <pearcem.dx@gmail.com>
2025-01-25 15:05:08 -0500

    players can now bonk each other

--- scripts/bubble_spawner.gd
+++ scripts/bubble_spawner.gd
@@ -3,19 +3,24 @@ extends Node2D
 
 @onready var timer: Timer = $"../Timer"
 
-@export var p0_life_object : PackedScene
-@export var p1_life_object : PackedScene
+@export var basic_bubble : Array[PackedScene]
+@export var life_bubbles : Array[PackedScene]
+@export var loot_bubbles : Array[PackedScene]
 
-@export var bubble_objects : Array[PackedScene]
 
 @export var bubble_cap : int = 25
 @export var bubble_ratio : int = 5
+
+@export var life_freq : int = 10
+@export var loot_freq : int = 12
 
 var rng = RandomNumberGenerator.new()
 var bubble_count : int = 0
 var normal_rate : float
 
-var life_timer = 10
+var life_timer = 0
+var loot_timer = 0
+
 var boost_timer = 15
 var boosting : bool = false
 
@@ -24,6 +29,9 @@ func _ready() -> void:
 	SignalBus.BubblePopped.connect(_on_bubble_popped)
 	
 	normal_rate = timer.wait_time
+	
+	life_timer = life_freq
+	loot_timer = loot_freq
 	
 	SignalBus.BubbleBoost.emit()
 	
@@ -34,7 +42,7 @@ func spawn_bubble(obj : PackedScene):
 	bubble.random_force = Vector2(randi_range(-20,20),randi_range(-20,20))
 	
 	add_child(bubble)
-	bubble.set_bubble_size(randf_range(0.5,2))
+	bubble.set_bubble_size(randf_range(0.5,1.5))
 
 func _on_bubble_boost():
 	boost_timer = 20
@@ -52,17 +60,27 @@ func _on_timer_timeout() -> void:
 		boosting = false
 		timer.wait_time = normal_rate
 	
+	# spawn life bubbles
 	if life_timer == 0:
-		spawn_bubble(p0_life_object)
-		spawn_bubble(p1_life_object)
-		life_timer = 10
+		spawn_bubble(life_bubbles[0])
+		spawn_bubble(life_bubbles[1])
+		life_timer = life_freq
 	else:
 		life_timer -= 1
+		
+	# spawn loot bubbles
+	if loot_timer == 0:
+		var index = rng.randi_range(0,loot_bubbles.size()-1)
+		spawn_bubble(loot_bubbles[index])
+		loot_timer = loot_freq
+	else:
+		loot_timer -= 1
 	
+	#spawn basic bubbles
 	if bubble_count < bubble_cap:
-		var obj_count = bubble_objects.size()
+		var obj_count = basic_bubble.size()
 		var index = rng.randi_range(0,obj_count+bubble_ratio)
 		if (index > obj_count-1): index = 0
-		spawn_bubble(bubble_objects[index])
+		spawn_bubble(basic_bubble[index])
 		bubble_count += 1
 		timer.start()
--- scripts/player.gd
+++ scripts/player.gd
@@ -12,45 +12,36 @@ extends CharacterBody2D
 var jump_bubble : BubbleJump
 var jump_dir : Vector2
 var move_dir : Vector2
-var facing_direction : int = 1; # Set based on player 
 var jump_power_value : float
 var active_animation : AnimatedSprite2D
-var roll_rotation_speed = 0.032; #0.008 for bubble mode
 
-enum player_states { Neutral, Charge, Dive, Roll, Bubbled, BubbledCharge }
+enum player_states { Neutral, Charge, Dive, Roll }
 var player_state : player_states = player_states.Neutral;
 
-#region Lifecycle Functions
 func _ready() -> void:
 	jump_power.value = 0
 	
 	if player_id == 0:
 		active_animation = red_sprites;
-		facing_direction =-1;
 	else:
 		active_animation = blue_sprites;
-		facing_direction = 1;
 	
 	active_animation.visible = true;
-	set_sprite_flip();
-	enter_neutral_state();
-
+		
 func _process(delta: float) -> void:
+	move_dir.x = Input.get_joy_axis(player_id,JOY_AXIS_LEFT_X)
+	jump_dir.x = Input.get_joy_axis(player_id,JOY_AXIS_RIGHT_X)
+	jump_dir.y = Input.get_joy_axis(player_id,JOY_AXIS_RIGHT_Y)
+	
+	jump_power.rotation = get_angle_to(global_position + jump_dir) + 80
+	
 	match(player_state):
-		player_states.Neutral:
-			update_neutral();
-		player_states.Charge:
-			update_charge();
 		player_states.Dive:
-			update_dive();
-		player_states.Roll:
-			update_roll();
-		player_states.Bubbled:
-			update_bubbled();
-		player_states.BubbledCharge:
-			update_bubbled_charge();
-			
+			active_animation.rotation = velocity.angle();
+
+
 func _physics_process(delta: float) -> void:
+	
 	if jump_bubble:
 		global_position = jump_bubble.global_position
 		velocity = Vector2.ZERO
@@ -63,111 +54,32 @@ func _physics_process(delta: float) -> void:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
 	else:
 		velocity.y += get_gravity().y * delta
-	
+			
+	if is_on_floor() || jump_bubble:
+		
+		# Charge jump
+		if Input.is_action_pressed("P%s_jump" % player_id):
+			if (jump_power_value < jump_power.max_value):
+				jump_power_value += 0.5
+				jump_power.value = jump_power_value
+		
+		# Execute jump
+		if Input.is_action_just_released("P%s_jump" % player_id):
+			
+			if jump_bubble:
+				jump_bubble.queue_free()
+			
+			velocity = jump_dir * JUMP_VELOCITY * jump_power_value
+			
+			player_state = player_states.Dive;
+			active_animation.play("Dive");
+			
+			jump_power_value = 0
+			jump_power.value = jump_power_value
+			
+			
+
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+
 	move_and_slide()
-#endregion
-
-#region Enter State
-func enter_neutral_state() -> void:
-	player_state = player_states.Neutral;
-	active_animation.play("Neutral");
-	active_animation.rotation = 0;
-
-func enter_charge_state() -> void:
-	player_state = player_states.Charge;
-	active_animation.play("Charge");
-	active_animation.rotation = 0;
-	jump_power_value = 0;
-	velocity.x = 0;
-	move_dir.x = 0;
-	
-func enter_dive_state() -> void:
-	player_state = player_states.Dive;
-	active_animation.play("Dive");
-	if jump_bubble:
-		jump_bubble.queue_free()
-	
-	velocity = jump_dir * JUMP_VELOCITY * jump_power_value
-	player_state = player_states.Dive;
-	active_animation.play("Dive");
-	jump_power_value = 0
-	jump_power.value = jump_power_value
-	
-func enter_bubbled_state() -> void:
-	player_state = player_states.Bubbled;
-	active_animation.play("Roll");
-	
-func enter_bubbled_charge_state() -> void:
-	player_state = player_states.BubbledCharge;
-	active_animation.play("Charge");
-	jump_power_value = 0;
-	velocity.x = 0;
-	move_dir.x = 0;
-#endregion
-
-#region Update State
-func update_neutral() -> void:
-	if Input.is_action_pressed("P%s_jump" % player_id):
-		enter_charge_state();
-		return;
-	move_dir.x = Input.get_joy_axis(player_id,JOY_AXIS_LEFT_X)
-	# update facing direction from aim
-	facing_direction = sign(move_dir.x);
-	set_sprite_flip();
-
-func update_charge() -> void:
-	if Input.is_action_just_released("P%s_jump" % player_id):
-		enter_dive_state();
-		return;
-	jump_dir.x = Input.get_joy_axis(player_id,JOY_AXIS_RIGHT_X)
-	jump_dir.y = Input.get_joy_axis(player_id,JOY_AXIS_RIGHT_Y)
-	jump_power.rotation = get_angle_to(global_position + jump_dir) + 80
-	if (jump_power_value < jump_power.max_value):
-		jump_power_value += 0.5
-		jump_power.value = jump_power_value
-
-func update_dive() -> void:
-	if jump_bubble:
-		enter_bubbled_state();
-	if is_on_floor():
-		enter_neutral_state();
-		return;
-	if sign(velocity.y) != -1:
-		player_state = player_states.Roll;
-		active_animation.play("Roll");
-	else:
-		active_animation.rotation = velocity.angle();
-
-func update_roll() -> void:
-	if jump_bubble:
-		enter_bubbled_state();
-	if is_on_floor():
-		enter_neutral_state();
-		return;
-	active_animation.rotation += roll_rotation_speed;
-	
-func update_bubbled() -> void:
-	if Input.is_action_pressed("P%s_jump" % player_id):
-		enter_bubbled_charge_state();
-		return;
-	# update facing direction from aim
-	facing_direction = sign(move_dir.x);
-	set_sprite_flip();
-	
-func update_bubbled_charge() -> void:
-	if Input.is_action_just_released("P%s_jump" % player_id):
-		enter_dive_state();
-		return;
-	jump_dir.x = Input.get_joy_axis(player_id,JOY_AXIS_RIGHT_X)
-	jump_dir.y = Input.get_joy_axis(player_id,JOY_AXIS_RIGHT_Y)
-	jump_power.rotation = get_angle_to(global_position + jump_dir) + 80
-	if (jump_power_value < jump_power.max_value):
-		jump_power_value += 0.5
-		jump_power.value = jump_power_value
-#endregion
-
-func set_sprite_flip() -> void:
-	if facing_direction == -1:
-		active_animation.flip_h = true;
-	else:
-		active_animation.flip_h = false;
